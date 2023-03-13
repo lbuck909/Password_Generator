3564eb30e41b563a91eb6abc4251b49604c4546f
LaiSha Buck <laishabuck@gmail.com>
2023-03-12 20:13:28 -0400

    updates to javascript

--- css/main.js
+++ css/main.js
@@ -1,11 +1,21 @@
 function getPassword() {
   const chars =
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+?><:{}[]";
+
   let passwordLength = 20;
-  let password = "";
+  let password = '';
 
   for (let i = 0; i < passwordLength; i++) {
     let randomNumber = Math.floor(Math.random() * chars.length);
     console.log(randomNumber);
+
+    password += chars.substring(randomNumber, randomNumber + 1);
+    console.log(password);
   }
+
+  document.getElementById("password").value = password;
+
+  
+    
+  
 }
