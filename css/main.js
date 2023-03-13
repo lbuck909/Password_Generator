b02f82ad19cc9fbb4072ed8ba036a26d2aee332f
LaiSha Buck <laishabuck@gmail.com>
2023-03-12 22:26:55 -0400

    javascript additions

--- css/main.js
+++ css/main.js
@@ -2,8 +2,8 @@ function getPassword() {
   const chars =
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+?><:{}[]";
 
-  let passwordLength = 20;
-  let password = '';
+  let passwordLength = 128;
+  let password = "";
 
   for (let i = 0; i < passwordLength; i++) {
     let randomNumber = Math.floor(Math.random() * chars.length);
@@ -12,10 +12,25 @@ function getPassword() {
     password += chars.substring(randomNumber, randomNumber + 1);
     console.log(password);
   }
-
+  
   document.getElementById("password").value = password;
+  var buttonSelector = document.getElementById("password");
 
-  
-    
-  
+
 }
+const element = document.getElementById("genButton");
+  element.addEventListener("click", getPassword);
+      //set value of DOM elements here//
+const passwordLength = document.getElementById("passwordLength");
+const numbers = document.getElementById("numbers");
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase")
+const symbols = document.getElementById("symbols");
+
+//place script for length specification here//
+genButton.addEventListener("click", () => {
+  let chars = chars;
+  numbers.checked ? (chars += numbers) : "";
+  symbols.checked ? (chars += symbols) : "";
+  passwordLength.value = generatePassword(length.value, chars);
+});
